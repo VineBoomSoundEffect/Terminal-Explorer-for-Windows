af99be5a231b09009e87a477f7636d9883732426
VineBoomSoundEffect <97389197+VineBoomSoundEffect@users.noreply.github.com>
2022-07-10 21:01:52 +0300

    Add files via upload

--- tex.cpp
+++ tex.cpp
@@ -5,6 +5,9 @@
 #include <dirent.h>
 #include <cstring>
 #include <conio.h>
+#include <fstream>
+
+#define Y y-1
 
 using namespace std;
 
@@ -27,6 +30,8 @@ int k; //return value of Show(), to enter folder or open file
 char input[300]; //string for input mode
 char command[350]; //string to execute a command
 char deletefile[350]; //string to warn user about deleting a file
+char path[MAX_PATH+1]; //stores the path to the executable location (without the executable name)
+int pathlen; //the length of the source path
 
 //FUNCTION TO OUTPUT IN DIFFERENT COLORS
 void Print(int color, string str){
@@ -34,6 +39,14 @@ void Print(int color, string str){
     cout << str;
     SetConsoleTextAttribute(h, 7);
 }
+
+//FUNCTION TO DELETE THE LAST CHARACTERS OF A STRING (unused)
+// char * last(char str[100], int l){
+	// for(int i=0;i<l;i++){
+		// str[strlen(str)-1] = '\0';
+	// }
+	// return str;
+// }
 
 //FUNCTION TO OUTPUT THE CONTENTS OF THE DIRECTORY
 int Show(char path[300]){
@@ -43,6 +56,7 @@ int Show(char path[300]){
     if(SetCurrentDirectory(path)) k = 1; //succeed -> open folder
     else k = 0; //fail -> open file
     system("cls");
+	system("cd");
     d = opendir(".");
     if(d){
         while((dir = readdir(d)) != NULL){
@@ -65,148 +79,53 @@ int Show(char path[300]){
     }
     return k;
 }
+
+//FUNCTION TO EXECUTE CODE FROM .TEX FILES
+void Compiler(char temp[255]){
+	char instruction[255];
+	char name[255];
+	char path_to_template[500];
+	strcpy(path_to_template, path);
+	strcat(path_to_template, temp);
+	strcat(path_to_template, ".tex");
+	ifstream fin(path_to_template);
+	while(fin >> instruction){
+			cout << "\n";
+		if(!strcmp(instruction, "dir")){
+			fin >> name;
+			strcpy(command, "mkdir ");
+			strcat(command, name);
+			system(command);
+		}
+		else if(!strcmp(instruction, "{")){
+			SetCurrentDirectory(name);
+		}
+		else if(!strcmp(instruction, "}")){
+			SetCurrentDirectory("..");
+		}
+		else if(!strcmp(instruction, "file")){
+			fin >> name;
+			strcpy(command, "type nul > \"");
+			strcat(command, name);
+			strcat(command, "\"");
+			system(command);
+		}
+	}
+}
+
 int main(int argc, char ** argv){
-    if(argv[1] != NULL){
-        if(!strcmp(argv[1], "help") && argc > 1){
-            cout << "File explorer with terminal interface\n";
-            cout << "W - move cursor up\n";
-            cout << "S - move cursor down\n";
-            cout << "ENTER - access folder/file\n";
-            cout << "T - manually type the folder/file you want to access\n";
-            cout << "N - create new empty file\n";
-            cout << "M - create new folder\n";
-            cout << "D - delete a file/folder\n";
-            cout << "\\ - close program\n\n";
-
-            cout << "write the command without arguments to open the current directory\n";
-            cout << "to open a specific directory write the path to it as argument\n";
-            return 0;
-        }
-        else{
-            SetCurrentDirectory(argv[1]);
-        }
-    }
+	//get the path of the executable location
+	GetModuleFileName(NULL, path, MAX_PATH);
+	pathlen = strlen(path);
+	while(path[pathlen] != '\\'){
+		path[pathlen] = '\0';
+		pathlen--;
+	}
+	#include "args.h"
     Show(".");
-    SetConsoleCursorPosition(h, {0, 0});
-    while(ch != '\\'){
-        ch = getch();
-        switch(ch){
-            case 'w':
-            case 'W':{
-                if(y != 0) y--;
-                SetConsoleCursorPosition(h, {0, y});
-                break;
-            }
-            case 's':
-            case 'S':{
-                y++;
-                SetConsoleCursorPosition(h, {0, y});
-                break;
-            }
-            case '\r':{
-                Show(files[y]);
-                if(k){
-                    y = 0;
-                    SetConsoleCursorPosition(h, {0, y});
-                }
-                else{
-                    strcpy(command, "\"");
-                    strcat(command, files[y]);
-                    strcat(command, "\"");
-
-                    system(command);
-                    SetConsoleCursorPosition(h, {0, y});
-                }
-                break;
-            }
-            case 't':
-            case 'T':{
-                Show(".");
-                SetConsoleTextAttribute(h, INPUT_COLOR);
-                cin.get(input, 300);
-                cin.get();
-                SetConsoleTextAttribute(h, 7);
-                Show(input);
-                if(k){
-                    y = 0;
-                    SetConsoleCursorPosition(h, {0, y});
-                }
-                else{
-                    strcpy(command, "\"");
-                    strcat(command, input);
-                    strcat(command, "\"");
-
-                    system(command);
-                    SetConsoleCursorPosition(h, {0, y});
-                }
-                break;
-            }
-            case 'n':
-            case 'N':{
-                Show(".");
-                SetConsoleTextAttribute(h, INPUT_COLOR);
-                cin.get(input, 300);
-                cin.get();
-                SetConsoleTextAttribute(h, 7);
-                strcpy(command, "type nul > \"");
-                strcat(command, input);
-                strcat(command, "\"");
-                system(command);
-                Show(".");
-                y = 0;
-                SetConsoleCursorPosition(h, {0, y});
-                break;
-            }
-            case 'm':
-            case 'M':{
-                Show(".");
-                SetConsoleTextAttribute(h, INPUT_COLOR);
-                cin.get(input, 300);
-                cin.get();
-                SetConsoleTextAttribute(h, 7);
-                strcpy(command, "mkdir \"");
-                strcat(command, input);
-                strcat(command, "\"");
-                system(command);
-                Show(".");
-                y = 0;
-                SetConsoleCursorPosition(h, {0, y});
-                break;
-            }
-            case 'd':
-            case 'D':{
-                Show(files[y]);
-                if(k){
-                    Show("..");
-                    strcpy(command, "rmdir /s \"");
-                    strcat(command, files[y]);
-                    strcat(command, "\"");
-
-                    SetConsoleTextAttribute(h, 6);
-                    system(command);
-                    SetConsoleTextAttribute(h, 7);
-                }
-                else{
-					strcpy(deletefile, files[y]);
-					strcat(deletefile, ", Are you sure you want to delete this file? (Y/N)?");
-					Print(6, deletefile);
-					cin.get(validation);
-					cin.get();
-					if(validation=='y' || validation=='Y'){
-						strcpy(command, "del \"");
-						strcat(command, files[y]);
-						strcat(command, "\"");
-
-						system(command);
-					}
-                }
-                Show(".");
-                y = 0;
-                SetConsoleCursorPosition(h, {0, y});
-                break;
-            }
-        }
-    }
+	y = 0;
+    SetConsoleCursorPosition(h, {0, y});
+	#include "input.h"
     system("cls");
     return 0;
 }
